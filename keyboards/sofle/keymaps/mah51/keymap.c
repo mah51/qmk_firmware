e4d03261ffaddbd26b638519315c5903c01bf826
mah51 <mikiemike.hall@gmail.com>
2023-01-17 01:36:32 +0000

    Changing lower to outer thumbs to bring it inline with sofle

--- keyboards/sofle/keymaps/mah51/keymap.c
+++ keyboards/sofle/keymaps/mah51/keymap.c
@@ -226,11 +226,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_COLEMAK_DH] = LAYOUT( \
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,     KC_5,                                KC_6,       KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
-  KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,     KC_B,                                KC_J,       KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_BSPC,
-  KC_RAISE, KC_A,   KC_R,    KC_S,    KC_T,     KC_G,                                KC_M,       KC_N,    KC_E,    KC_I,    KC_O,  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,     KC_V,     KC_MUTEPLAY,  KC_LIGHTS,   KC_K,       KC_H,    KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
-                    KC_LALT, KC_SPC,  KC_LCTL,  KC_LOWER, KC_ENT,          KC_SPC,   KC_LOWER,   KC_RCTL,KC_RALT, KC_RGUI
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,     KC_5,                                KC_6,       KC_7,    KC_8,    KC_9,   KC_0,  KC_GRV,
+  KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,     KC_B,                                KC_J,       KC_L,    KC_U,    KC_Y,   KC_SCLN,  KC_BSPC,
+  KC_RAISE, KC_A,   KC_R,    KC_S,    KC_T,     KC_G,                                KC_M,       KC_N,    KC_E,    KC_I,   KC_O,  KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,     KC_V,   KC_MUTEPLAY,    KC_LIGHTS,   KC_K,       KC_H,    KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
+                    KC_LALT, KC_SPC,  KC_LCTL,  KC_ENT, KC_LOWER,       KC_LOWER,    KC_SPC,     KC_RCTL, KC_RALT, KC_RGUI
 ),
 
 /*
@@ -250,11 +250,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,     KC_3,     KC_4,     KC_5,                               KC_6,      KC_7,      KC_8,     KC_9,   KC_0,    KC_GRV,
-   KC_TAB,   KC_Q,   KC_W,     KC_E,     KC_R,     KC_T,                               KC_Y,      KC_U,      KC_I,     KC_O,   KC_P,    KC_BSPC,
-   KC_RAISE, KC_A,   KC_S,     KC_D,     KC_F,     KC_G,                               KC_H,      KC_J,      KC_K,     KC_L,   KC_SCLN, KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,     KC_C,     KC_V,     KC_B,     KC_MUTEPLAY,  KC_LIGHTS,  KC_N,      KC_M,      KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
-                     KC_LALT,  KC_SPC,   KC_LCTL, KC_LOWER, KC_ENT,        KC_SPC,    KC_LOWER,  KC_RCTL,  KC_RALT,  KC_RGUI
+  KC_ESC  , KC_1, KC_2   , KC_3  , KC_4   , KC_5  ,                          KC_6  , KC_7   , KC_8   , KC_9  , KC_0   , KC_GRV,
+  KC_TAB  , KC_Q, KC_W   , KC_E  , KC_R   , KC_T  ,                          KC_Y  , KC_U   , KC_I   , KC_O  , KC_P   , KC_BSPC,
+  KC_RAISE, KC_A, KC_S   , KC_D  , KC_F   , KC_G  ,                          KC_H  , KC_J   , KC_K   , KC_L  , KC_SCLN, KC_QUOT,
+  KC_LSFT , KC_Z, KC_X   , KC_C  , KC_V   , KC_B  , KC_MUTEPLAY,  KC_LIGHTS, KC_N  , KC_M   , KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+                  KC_LALT, KC_SPC, KC_LCTL, KC_ENT, KC_LOWER   ,  KC_LOWER , KC_SPC, KC_RCTL, KC_RALT, KC_RGUI
 ),
     /* EMOJI TEXT
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -318,8 +318,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT(
 KC_F12,  KC_F1,     KC_F2,      KC_F3,      KC_F4,      KC_F5,                              KC_F6,      KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,
 _______, KC_1,      KC_2,       KC_3,       KC_4,       KC_5,                               KC_6,       KC_7,      KC_8,      KC_9,      KC_0,      KC_DEL,
-_______, KC_DLR,    KC_PLUS,    KC_LPRN,    KC_RPRN,    KC_AT,                              KC_EXLM,    KC_EQL,    KC_MINS,   KC_UNDS,   KC_ASTR,   X(PND),
-_______, KC_EXLM,   KC_HASH,    KC_LCBR,    KC_RCBR,    KC_TILDE,  _______,     _______,    KC_AMPR,    KC_LBRC,   KC_RBRC,   KC_PERC,   KC_BSLS,   _______,
+_______, KC_DLR,    KC_PLUS,    KC_LPRN,    KC_RPRN,    KC_AT,                              KC_EXLM,    KC_EQL,    KC_MINS,   KC_UNDS,   KC_ASTR,   KC_GRV,
+_______, KC_EXLM,   KC_HASH,    KC_LCBR,    KC_RCBR,    KC_TILDE,  _______,     _______,    KC_AMPR,    KC_LBRC,   KC_RBRC,   KC_PERC,   KC_BSLS,   X(PND),
                     _______,    _______,    _______,    _______,   _______,     _______,    _______,  _______,  _______,  _______
 ),
 /* RAISE
